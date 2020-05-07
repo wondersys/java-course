9806ed6b14d0fe3bca22089a32c0c36869f9a362
Luigi Fortunati <l.fortunati@roomstock.biz>
2020-05-07 21:43:46 +0200

    First batch of material

--- java-basic/src/it/wondersys/example/types/ReferenceTypes.java
+++ java-basic/src/it/wondersys/example/types/ReferenceTypes.java
@@ -8,14 +8,14 @@ public class ReferenceTypes {
 
     public static void main(String[] args) {
         byte age = 36;
-        //IntelliSense should suggest available types while typing Date
 
+        //IntelliSense should suggest available types while typing Date
         //now is a Reference type variable with type Date
         Date now = new Date(); //'new' keyword allows to create new instances of a class
 
-        long millisSince1970 = now.getTime(); // Members can be accessed with the dot notation
+        long millisSince1970 = now.getTime(); // Members can be accessed with the dot notation, check the documentation
 
-        System.out.println("Millis since 1970: " + now.getTime());
+        System.out.println("Millis since 1970: " + millisSince1970);
         System.out.println(now);
 
         final Cat cat = new Cat(8, 4.1);
--- java-basic/src/it/wondersys/example/types/stack/reference/Cat.java
+++ java-basic/src/it/wondersys/example/types/stack/reference/Cat.java
@@ -2,20 +2,34 @@ package it.wondersys.example.types.stack.reference;
 
 public class Cat {
 
+    //These are called fields, this is the state of every instance a Cat will have
     int age;
     double weight;
 
+    //This is called constructor. If any is defined, default constructor (with no params) will be available
     public Cat(int age, double weight) {
         this.age = age;
         this.weight = weight;
     }
 
+    //These are methods and builds up the behavior each instance of Cat will share
+
+    //Methods that provides access to the state are called getters
     public int getAge() {
         return age;
     }
 
     public double getWeight() {
         return weight;
+    }
+
+    //You can also have setters to provide a change of state
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
     }
 
     @Override
